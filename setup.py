c878941e02e9b99b4d5d5e6d72a0b37dd266fdcb
Tommi Hyppänen <thyppane@gmail.com>
2023-09-12 19:22:38 +0300

    Initial

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+# python setup.py sdist
+# pip install ./dist/convo-0.1.0.tar.gz
+
+setup(
+    name="convo",
+    version="0.1.0",
+    py_modules=["convo"],
+    entry_points={"console_scripts": ["convo=convo:main"]},
+)
